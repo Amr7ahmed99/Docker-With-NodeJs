409ca5e70910b8382be9ccbd5dd540c95cca7a5f
Amr7ahmed99 <amr.omar@maxab.io>
2023-09-09 19:28:25 +0300

    Merge branch 'docker-compose'

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express= require("express");
+const port= 4000;
+const app= express();  
+
+app.get('/', (req, res)=> res.send("<h1> Hello Node, I'm docker</h1>"));
+
+app.listen(port, ()=> console.log(`app is up and running on port: ${port}`));
