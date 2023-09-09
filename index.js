fe35c193f4a6ba18a868aca48b9ed22bbe62fa80
Amr7ahmed99 <amr.omar@maxab.io>
2023-09-09 19:12:11 +0300

    create docker-compose with bind mount (one way binding) and port forwarding.

--- index.js
+++
@@ -1,7 +0,0 @@
-const express= require("express");
-const port= 4000;
-const app= express();  
-
-app.get('/', (req, res)=> res.send("<h1> Hello Node </h1>"));
-
-app.listen(port, ()=> console.log(`app is up and running on port: ${port}`));
