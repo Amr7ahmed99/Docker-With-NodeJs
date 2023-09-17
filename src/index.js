c4f052979db884744129aee97c99d7e2926e58d0
Amr7ahmed99 <amr.omar@maxab.io>
2023-09-17 16:57:54 +0300

    Integrating with mongoDB and mongo-express.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express= require("express");
 const port= process.env.PORT || 4000;
 const app= express();  
+const mongoose= require('mongoose');
+
+const DB_USER= 'root';
+const DB_PASSWORD= 'example';
+const DB_PORT= 27017;
+const DB_HOST= 'mongo';
+mongoose
+    .connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`)
+    .then( _=> console.log('connected to db.'))
+    .catch(err=> console.log('failed to connect with db.', err));
 
 app.get('/', (req, res)=> res.send("<h1> Hello Node, I'm docker</h1>"));
 
